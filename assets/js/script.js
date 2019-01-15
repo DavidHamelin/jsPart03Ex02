78cf9a4a4414e1636b43b2fd5b4500f15ce23794
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-15 15:26:17 +0100

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-﻿function showMessage() {
-    document.getElementById("text").style.display = "block";
+﻿function showMessage() { // on déclare la fonction ici
+    document.getElementById("text").style.display = "initial"; // display en css permet de modifier le type d'élément (block, inline, flex, initial ou none) 
 }
 function hideMessage() {
-    document.getElementById("text").style.display = "none";
+    document.getElementById("text").style.display = "none"; // ici l'élément disparait
 }
